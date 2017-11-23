28bd70580493868782729013e15d88e02cc12511
Kamil Kawa <kawa@student.agh.edu.pl>
2017-11-23 14:53:12 +0100

    Login view added to the Project. User model also configured

--- MPT/MPT/Controllers/HomeController.cs
+++ MPT/MPT/Controllers/HomeController.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MPT.Models;
 
 namespace MPT.Controllers
 {
     public class HomeController : Controller
     {
+       public ActionResult Login()
+        {
+            User usr = new User();
+            if (Request.Cookies["userInfp"] != null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(usr);
+        }
+        
+        //action after login need to be applied. We have to find in a databe a proped id. Password should be hashed. It could be added, after database creation
+        [HttpPost]
+        public ActionResult Login(string ID, string HasPassword) // this method could also contain a permissions.
+        {
+            return View();
+  
+        }
+        
+        
+        
         // GET: Home
         public ActionResult Index()
         {
--- MPT/MPT/Models/User.cs
+++ MPT/MPT/Models/User.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MPT.Models
 {
     public class User
     {
+        [Key]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string HasPassword { get; set; }
     }
 }
