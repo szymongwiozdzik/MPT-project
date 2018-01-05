531a976b65d62973189824b301c68db2eec9b405
Kamil Kawa <kawa@student.agh.edu.pl>
2018-01-05 01:21:19 +0100

    Deleting an event is working properly. However Save button cause failure! Can not create/modify an event

--- MPT/MPT/Controllers/HomeController.cs
+++ MPT/MPT/Controllers/HomeController.cs
@@ -131,10 +131,55 @@ namespace MPT.Controllers
             }
         }
 
-        
-        
-        
-        
+        [HttpPost]
+        public JsonResult SaveEvent(Events e)
+        {
+            var status = false;
+            using (MPTDatabaseEntities dc = new MPTDatabaseEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (MPTDatabaseEntities dc = new MPTDatabaseEntities())
+            {
+                var v = dc.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+
+
         // GET: Home
         public ActionResult Index()
         {
